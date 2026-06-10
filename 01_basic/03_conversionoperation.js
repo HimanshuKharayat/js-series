8ca0cb70a3b4b179d51416677ec53219ca7fcf26
HimanshuKharayat <kharayathimanshu@gmail.com>
2026-06-10 02:36:01 +0000

    operation

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,68 @@
+let score = true ;
+
+console.log(typeof score) ;
+console.log(typeof(score)) ;
+
+let valueInNumber = Number(score) ;
+console.log(typeof valueInNumber) ;
+console.log(valueInNumber)  
+
+// "33" => 33 convert to value
+// "33abc" => Nan (not a number) 
+// true => 1 
+
+let isLoggedIn = 1 ;
+
+let booleanIsloggedIn = Boolean(isLoggedIn) ;
+console.log(booleanIsloggedIn) 
+
+// 1 => true , 0 => false 
+// "" => false 
+// "himu" => true
+
+let somenumber = 0 ; 
+
+let stringnumber = String(somenumber) ;
+console.log(stringnumber) ;
+console.log(typeof stringnumber) 
+
+//operations
+
+let value = 2 
+let negvalue = -value 
+console.log(negvalue) 
+
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2%2)
+
+let str1 = "hello" ;
+let str2 = "himu" ; 
+ let str3 = str1 + str2 ;
+ console.log(str3) ; 
+
+ console.log("2" + 3) ;
+ console.log("2" +"2"+ "3") ;
+ console.log("2" + 3 +5) ;
+ console.log(2 + 3 +"5") ;
+
+
+ console.log(+true)
+ console.log(+"")
+
+ let num1 , num2 , num3 ;
+
+ num1 = num2 = num3 = 2+2 ;
+
+ console.log(num1,num2,num3) ;  
+
+ let gamecounter = 100 ; 
+ //gamecounter++ ; 
+ ++gamecounter ;
+ console.log(gamecounter) ; 
+
+
+
