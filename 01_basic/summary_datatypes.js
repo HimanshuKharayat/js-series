82fcdd2ea9ea33fac1cadc7772b99e54eeef8ca7
HimanshuKharayat <kharayathimanshu@gmail.com>
2026-06-11 08:32:43 +0000

    datatypesummary

---
+++ 01_basic/summary_datatypes.js
@@ -0,0 +1,35 @@
+// js is dynamically type language .
+// premitive  => callback value 
+
+//7 types  string , number , boolean , null , undefine , symbol , bigInt 
+const score = 100 ;
+const scorevalue = 100.3;
+const isloggedIn = false  ; 
+let useremail ;
+
+const id = Symbol("123") ;
+const anotherid = Symbol("123") ;
+
+console.log(id === anotherid) ;
+
+const bigNumber = 2344343n ;
+
+// reference (non premitive)
+
+// array , functions, objects
+
+
+const heros = ["saktiman " , "naag" , "doga" ]
+let myobj = {
+    name: "himu" ,
+   // age = 23 ,
+} // objects are in under the curly braces
+
+const myfunction = function(){
+    console.log("hello") ; 
+}
+
+
+console.log(typeof bigNumber);
+
+
