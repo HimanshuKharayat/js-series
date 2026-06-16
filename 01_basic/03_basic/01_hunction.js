d099aa06748e94749687bf1369e9fada0b7f2326
HimanshuKharayat <kharayathimanshu@gmail.com>
2026-06-16 18:37:33 +0000

    function

--- 01_basic/03_basic/01_hunction.js
+++ 01_basic/03_basic/01_hunction.js
@@ -4,9 +4,34 @@ function saymyname(){
      console.log("m");
      console.log("u");
 }
-saymyname() ;
+//saymyname() ;
+
+// function addtwonumbers(number1 , number2){ // (number1 , number2 ) parameters
+//     console.log(number1 + number2);
+// }
+//  addtwonumbers(3,5) ; // (3,5) arguments
+
+//  const result = addtwonumbers(3,5) ;
+//  console.log("result:",result) ;
 
 function addtwonumbers(number1 , number2){
-    console.log(number1 + number2);
+    //let result = number1 + number2 ;
+    //return result ;
+    //  OR 
+    return number1+number2 ;
 }
-addtwonumbers(3,"a") ;
+ 
+ const result = addtwonumbers(3,5) ;
+ //console.log("result:",result) ;
+
+ function loginusermessage(username){
+    if(!username){
+       console.log("please enter a username");
+       return ;
+    }
+    return `${username} just log in` ;
+
+ }
+
+ //console.log(loginusermessage("himu")) ;
+  console.log(loginusermessage())
