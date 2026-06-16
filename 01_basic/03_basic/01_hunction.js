8682c2e4f1c42e1d1054aff621dd611b08ed01be
HimanshuKharayat <kharayathimanshu@gmail.com>
2026-06-16 17:39:22 +0000

    function

---
+++ 01_basic/03_basic/01_hunction.js
@@ -0,0 +1,12 @@
+function saymyname(){
+     console.log("h");
+     console.log("i");
+     console.log("m");
+     console.log("u");
+}
+saymyname() ;
+
+function addtwonumbers(number1 , number2){
+    console.log(number1 + number2);
+}
+addtwonumbers(3,"a") ;
