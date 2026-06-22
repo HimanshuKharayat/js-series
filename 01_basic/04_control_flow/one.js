4532268cf22a72db02bb09bfaeeca9020bb99d72
HimanshuKharayat <kharayathimanshu@gmail.com>
2026-06-22 06:51:51 +0000

    ifelse

--- 01_basic/04_control_flow/one.js
+++ 01_basic/04_control_flow/one.js
@@ -1,3 +1,52 @@
-const isloggedin  = function(){
-    if(2 == 2)
+// if 
+
+// const temperatue = 43  ;
+// if(temperatue === 40){
+//     console.log("less than 50") ;
+// }
+// else{
+//     console.log("temperatue is greater than 50" ) ;
+// }
+
+//  < , > , <= , >= , == , === strict checking type , != , !==  
+
+// const score = 200 ; 
+// if(score > 100){
+//     const power = "fly" ;  // do not use var because it  is complete global scope 
+//     console.log(`user power : ${power}`) ;
+// }
+// console.log(`user power : ${power}`) ;   
+
+const balance = 1000 ; 
+
+//if(balance > 500) console.log("test") ;    // implicit scope 
+
+// Nesting
+
+// if(balance < 500){
+//     console.log("less than 500") ;
+// }
+// else if(balance < 750){
+//     console.log("less than 750")
+// }
+// else {
+//     console.log("less than 1200") ;
+// }
+
+
+const userloggedin  = true ; 
+const userloginfromgoogle  = false ;
+const userloginfromemail = true ;
+
+if(userloggedin && userloginfromgoogle){
+    console.log("allow login") ;
 }
+if(userloggedin || userloginfromgoogle){
+    console.log("allow login") ;
+}
+
+
+
+
+
+
