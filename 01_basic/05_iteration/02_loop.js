88321a68b373a046020776fe0a96dbea2bebdf0f
HimanshuKharayat <kharayathimanshu@gmail.com>
2026-06-25 18:38:09 +0000

    dowhile

--- 01_basic/05_iteration/02_loop.js
+++ 01_basic/05_iteration/02_loop.js
@@ -4,3 +4,15 @@ while(index <= 10){
     index = index+2 ; 
 
 }
+
+let myarray = ["superman" , "batman" , "spiderman"] ;
+let arr = 0 ; 
+while(arr < myarray.length ){
+    console.log(`value is ${myarray[arr]}`) ;
+    arr = arr+1 ; 
+}
+let score = 11 ; 
+do {
+     console.log(`score is ${score}`);
+     score++
+}while(score <= 10) ;
